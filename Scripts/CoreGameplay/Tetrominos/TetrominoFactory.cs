8d4e973719f8d73021361686f6ec93daf463070d
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2023-11-16 19:27:07 +0200

    added othert tetromino

--- Scripts/CoreGameplay/Tetrominos/TetrominoFactory.cs
+++ Scripts/CoreGameplay/Tetrominos/TetrominoFactory.cs
@@ -6,16 +6,119 @@ namespace GodotTetris.Scripts;
 
 public static class TetrominoFactory {
 	static List<Func<Tetromino>> _tetrominoCreators = new() {
-		// CreateSquare,
-		// CreateT,
-		CreateI
+		CreateL,
+		CreateJ,
+		CreateI,
+		CreateSquare,
+		CreateZ,
+		CreateS,
+		CreateT
 	};
 	
 	public static Tetromino CreateRandomTetromino() {
 		var randomIndex = (int) (GD.Randi() % _tetrominoCreators.Count);
 		return _tetrominoCreators[randomIndex].Invoke();
 	}
-
+	static Tetromino CreateJ() {
+		var tetromino = new Tetromino();
+		tetromino.AddForm(
+			new TetrominoForm(new[] {
+				" X",
+				" X",
+				"XX",
+			})
+		);
+		tetromino.AddForm(
+			new TetrominoForm(new[] {
+				"X  ",
+				"XXX",
+			})
+		);
+		tetromino.AddForm(
+			new TetrominoForm(new[] {
+				" XX",
+				" X ",
+				" X ",
+			})
+		);
+		tetromino.AddForm(
+			new TetrominoForm(new[] {
+				"   ",
+				"XXX",
+				"  X",
+			})
+		);
+		return tetromino;
+	}
+	static Tetromino CreateL() {
+		var tetromino = new Tetromino();
+		tetromino.AddForm(
+			new TetrominoForm(new[] {
+				" X ",
+				" X ",
+				" XX",
+			})
+		);
+		tetromino.AddForm(
+			new TetrominoForm(new[] {
+				"   ",
+				"XXX",
+				"X  ",
+			})
+		);
+		tetromino.AddForm(
+			new TetrominoForm(new[] {
+				"XX",
+				" X",
+				" X",
+			})
+		);
+		tetromino.AddForm(
+			new TetrominoForm(new[] {
+				"  X",
+				"XXX",
+			})
+		);
+		return tetromino;
+	}
+	
+	
+	static Tetromino CreateZ() {
+		var tetromino = new Tetromino();
+		tetromino.AddForm(
+			new TetrominoForm(new[] {
+				"XX ",
+				" XX",
+			})
+		);
+		tetromino.AddForm(
+			new TetrominoForm(new[] {
+				"  X",
+				" XX",
+				" X ",
+			})
+		);
+		return tetromino;
+	}
+	
+	static Tetromino CreateS() {
+		var tetromino = new Tetromino();
+		tetromino.AddForm(
+			new TetrominoForm(new[] {
+				" XX",
+				"XX ",
+			})
+		);
+		tetromino.AddForm(
+			new TetrominoForm(new[] {
+				"X ",
+				"XX",
+				" X",
+			})
+		);
+		return tetromino;
+	}
+	
 	static Tetromino CreateI() {
 		var tetromino = new Tetromino();
 		tetromino.AddForm(
